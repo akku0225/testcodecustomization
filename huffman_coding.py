cdd59057765572a561454ab221e121a03a92da59
akku0225 <66835544+akku0225@users.noreply.github.com>
2025-04-23 11:31:38 +0530

    Update huffman_coding.py

--- huffman_coding.py
+++ huffman_coding.py
@@ -6,25 +6,23 @@ Date Updated: 3 Oct 2022
 from heapq import *
 from collections import Counter
 
-s = "Hello Huffman"
+s1 = "Hello Huffman"
 
 def huffman_coding(s):
-    freq = Counter(s)
+    f = Counter(s)
 
-    heap = [[f, [char, ""]] for f, char in freq.items()]
-    heapify(heap)
+    heap1 = [[f, [char, ""]] for f, char in f.items()]
+    heapify(heap1)
 
-    while len(heap) > 1:
-        right = heappop(heap) # 0
-        left = heappop(heap) # 1
+    while len(heap1) > 1:
+        right = heappop(heap1) # 0
+        left = heappop(heap1) # 1
 
         for pair in right[1:]:
             pair[1] = '0' + pair[1]
         for pair in left[1:]:
             pair[1] = '1' + pair[1]
 
-        heappush(heap, ([right[0] + left[0]] + right[1:] + left[1:]))
+        heappush(heap1, ([right[0] + left[0]] + right[1:] + left[1:]))
 
-    return heap
-
-print(huffman_coding(s))
+    return heap1
